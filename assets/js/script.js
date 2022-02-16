baf59b4ce1982dcf894142f5e13acd13aa88db1a
DonaldVN <vietplqbdaf200035@fpt.edu.vn>
2022-02-16 18:16:37 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,228 @@
+/**
+ * 1. RENDER SONGS
+ * 2. SCROLL TOP
+ * 3. PLAY, PAUSE, SEEK
+ * 4. CD ROTATE
+ * 5. NEXT / PREV
+ * 6. RANDOM
+ * 7 NEXT / REPEAT WHEN ENDED
+ * 8 ACITVE SONG
+ * 9. SCROLL ACTIVE SONG INTO VIEW
+ * 10 PLAY SONG WHEN CLICK
+ */
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const playlist = $('.js-playlist');
+const songName = $('.dashboard__song h3');
+const songImg = $('.dashboard__song img');
+const playBtn = $('.dashboard__play');
+const playerBtn = $('.dashboard__player');
+const playing = $('.js-playing');
+const nextBtn = $('.js-next');
+const prevBtn = $('.js-prev');
+
+const audio = $('#audio');
+const progress = $('#progress');
+
+const app = {
+    isPlaying: false,
+    currentIndex: 0,
+    songs: [{
+            name: 'Bài ca tuổi trẻ',
+            singer: 'DonaldVN',
+            path: './assets/music/Bai-Ca-Tuoi-Tre-DJ-Minh-Tri-Remix-DJ-Minh-Tri.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Bánh mì Không',
+            singer: 'Đạt G - Phương Uyên',
+            path: './assets/music/Banh-Mi-Khong-Dat-G-DuUyen.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Có anh ở đây rồi',
+            singer: 'DonaldVN',
+            path: './assets/music/Co-Anh-O-Day-Roi-Dj-Thanh-Nguyen-ft-Anh-Chau-Remix-Anh-Quan-Idol.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Dường như thói quen',
+            singer: 'DonaldVN',
+            path: './assets/music/Dung-Nhu-Thoi-Quen-JayKii-Sara-Luu.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Em ổn không?',
+            singer: 'DonaldVN',
+            path: './assets/music/Em-On-Khong-Trinh-Thien-An-ViruSs.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'End of time',
+            singer: 'DonaldVN',
+            path: './assets/music/End-Of-Time-K-391-Alan-Walker-Ahrix.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'How do you like that',
+            singer: 'DonaldVN',
+            path: './assets/music/HowYouLikeThat-BLACKPINK-6720100.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'I lose myself',
+            singer: 'DonaldVN',
+            path: './assets/music/If-I-Lose-Myself-Madilyn-Bailey.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Lemon tree',
+            singer: 'DonaldVN',
+            path: './assets/music/Lemon+Tree.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Memories',
+            singer: 'DonaldVN',
+            path: './assets/music/Memories-Maroon5-6091839.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'My love',
+            singer: 'DonaldVN',
+            path: './assets/music/My Love - Westlife.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Unfollow',
+            singer: 'DonaldVN',
+            path: './assets/music/Unfollow-Tua-Freaky-CM1X.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Vợ tuyệt vời nhất',
+            singer: 'DonaldVN',
+            path: './assets/music/Vo-Tuyet-Voi-Nhat-Vu-Duy-Khanh.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'Yêu rồi',
+            singer: 'DonaldVN',
+            path: './assets/music/Yeu-Roi-Gao-Nep-Gao-Te-OST-Tino.mp3',
+            img: './assets/img/LogoV.png',
+        },
+        {
+            name: 'You are my everything',
+            singer: 'DonaldVN',
+            path: './assets/music/You Are My Everything - Billkin',
+            img: './assets/img/LogoV.png',
+        },
+    ],
+    render: function() {
+        const htmls = this.songs.map((song) => {
+            return `<li class="playlist__song">
+                        <a href="#">
+                            <div class="playlist__infor">
+                                <img src="${song.img}" alt="" />
+                                <div class="playlist__name">
+                                    <h4>${song.name}</h4>
+                                    <h6>${song.singer}</h6>
+                                </div>
+                            </div>
+                            <i class="fas fa-ellipsis-h"></i>
+                        </a>
+                    </li>`;
+        });
+        playlist.innerHTML = htmls.join('');
+    },
+    defineProperties: function() {
+        Object.defineProperty(this, 'currentSong', {
+            get: function() {
+                return this.songs[this.currentIndex];
+            },
+        });
+    },
+    handleEvents: function() {
+        const _this = this;
+        const songImgAnimate = songImg.animate(
+            [{ transform: 'rotate(360deg)' }], {
+                duration: 20000,
+                iterations: Infinity,
+            }
+        );
+        songImgAnimate.pause();
+        playerBtn.onclick = function() {
+            if (_this.isPlaying) {
+                audio.pause();
+            } else {
+                audio.play();
+            }
+        };
+        audio.onplay = function() {
+            _this.isPlaying = true;
+            playerBtn.classList.add('js-playing');
+            songImgAnimate.play();
+        };
+        audio.onpause = function() {
+            _this.isPlaying = false;
+            playerBtn.classList.remove('js-playing');
+            songImgAnimate.pause();
+        };
+        audio.ontimeupdate = function() {
+            if (audio.duration) {
+                const progressPercent = Math.floor(
+                    (audio.currentTime / audio.duration) * 100
+                );
+                progress.value = progressPercent;
+            }
+        };
+
+        progress.onchange = function(e) {
+            const seekTime = (audio.duration / 100) * e.target.value;
+            audio.currentTime = seekTime;
+
+            console.log();
+        };
+        nextBtn.onclick = function() {
+            _this.nextSong();
+            audio.play();
+        };
+        prevBtn.onclick = function() {
+            _this.prevSong();
+            audio.play();
+        };
+    },
+    loadCurrentSong: function() {
+        songName.textContent = this.currentSong.name;
+        songImg.src = this.currentSong.img;
+        audio.src = this.currentSong.path;
+
+        // console.log(songImg, songName, audio);
+    },
+    nextSong: function() {
+        this.currentIndex++;
+        if (this.currentIndex >= this.songs.length) {
+            this.currentSong = 0;
+        }
+        this.loadCurrentSong();
+    },
+    prevSong: function() {
+        this.currentIndex--;
+        if (this.currentIndex < 0) {
+            this.currentSong = this.songs.length - 1;
+        }
+        this.loadCurrentSong();
+    },
+    start: function() {
+        this.defineProperties();
+
+        this.handleEvents();
+
+        this.loadCurrentSong();
+
+        this.render();
+    },
+};
+
+app.start();
---
+++ structure.md
@@ -0,0 +1,12 @@
+<!-- TEMPLATE
+
+link image: https://static.collectui.com/shots/3671744/musicloud-revolutionary-app-for-music-streaming-large
+
+link original: https://codepen.io/ng-ngc-sn-the-bashful/pen/oNYYLRp
+full link: https://codepen.io/ng-ngc-sn-the-bashful/pen/rNWWLWN
+
+ANIMATION: https://laphuocloc.github.io/Mp3Player/
+
+HOAN: https://hoan248.github.io/Music/?fbclid=IwAR0DKfFB57EkVYMnk0SptueEiH4Sgppw3Nys5PqLToxgGmjuL1-sMC4G0Y4
+
+-->
